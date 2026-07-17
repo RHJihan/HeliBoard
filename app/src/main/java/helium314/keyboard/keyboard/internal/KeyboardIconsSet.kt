db4264d95ac945281af251e807f438e1746effff
Md. Rifat Hasan Jihan <31377578+RHJihan@users.noreply.github.com>
2026-07-17 12:54:08 +0600

    add new icon for split keyboard

--- app/src/main/java/helium314/keyboard/keyboard/internal/KeyboardIconsSet.kt
+++ app/src/main/java/helium314/keyboard/keyboard/internal/KeyboardIconsSet.kt
@@ -154,7 +154,7 @@ class KeyboardIconsSet private constructor() {
                     ToolbarKey.FULL_RIGHT -> R.drawable.ic_to_end
                     ToolbarKey.PAGE_START -> R.drawable.ic_page_start
                     ToolbarKey.PAGE_END -> R.drawable.ic_page_end
-                    ToolbarKey.SPLIT -> R.drawable.ic_ime_switcher
+                    ToolbarKey.SPLIT -> R.drawable.ic_split_keyboard
                     ToolbarKey.BACKGROUND_GATHERING -> R.drawable.ic_settings_gesture
                 })
             }
@@ -217,7 +217,7 @@ class KeyboardIconsSet private constructor() {
                     ToolbarKey.FULL_RIGHT -> R.drawable.ic_to_end
                     ToolbarKey.PAGE_START -> R.drawable.ic_page_start
                     ToolbarKey.PAGE_END -> R.drawable.ic_page_end
-                    ToolbarKey.SPLIT -> R.drawable.ic_ime_switcher
+                    ToolbarKey.SPLIT -> R.drawable.ic_split_keyboard
                     ToolbarKey.BACKGROUND_GATHERING -> R.drawable.ic_settings_gesture
                 })
             }
@@ -280,7 +280,7 @@ class KeyboardIconsSet private constructor() {
                     ToolbarKey.FULL_RIGHT -> R.drawable.ic_to_end_rounded
                     ToolbarKey.PAGE_START -> R.drawable.ic_page_start_rounded
                     ToolbarKey.PAGE_END -> R.drawable.ic_page_end_rounded
-                    ToolbarKey.SPLIT -> R.drawable.ic_ime_switcher
+                    ToolbarKey.SPLIT -> R.drawable.ic_split_keyboard
                     ToolbarKey.BACKGROUND_GATHERING -> R.drawable.ic_settings_gesture
                 })
             }
